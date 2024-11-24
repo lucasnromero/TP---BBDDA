897b85936db3e3d5153d147cda14aed7eda1b5cb
Luciano Javier Romano <111657020+romano-luciano@users.noreply.github.com>
2024-11-23 23:53:27 -0300

    Update and rename 11-ScriptEliminarClientes.sql to 04-ScriptEliminaciones.sql
    Update and Rename

---
+++ stored procedures/04-ScriptEliminaciones.sql
@@ -0,0 +1,146 @@
+--Nos posicionamos en la base de datos
+use g05com2900
+go
+
+--Creamos los store procedures de eliminacion para el esquema de clientes
+
+--Creamos el store procedura para eliminar un tipo de cliente
+create or alter procedure clientes.EliminarTipoDeCliente
+    @id int
+as
+begin
+    --Verificamos si el tipo de cliente existe
+    if not exists(select 1 from clientes.TipoDeCliente where id = @id)
+    begin
+        print 'No se encontró el tipo de cliente con el ID especificado.';
+        return;
+    end
+    --Eliminamos el tipo de cliente
+    delete from clientes.TipoDeCliente where id = @id;
+    print 'Tipo de cliente eliminado correctamente con ID: ' + cast(@id as varchar(4));
+end;
+go
+
+--Creamos el store procedure para eliminar una ciudad
+create or alter procedure clientes.EliminarCiudad
+    @id int
+as
+begin
+    --Verificamos si la ciudad existe
+    if not exists(select 1 from clientes.Ciudad where id = @id)
+    begin
+        print 'No se encontró la ciudad con el ID especificado.';
+        return;
+    end
+    --Eliminamos la ciudad
+    delete from clientes.Ciudad where id = @id;
+
+    print 'Ciudad eliminada correctamente con ID: ' + cast(@id as varchar(4));
+end;
+go
+
+--Creamos el store procedure para eliminar un género
+create or alter procedure clientes.EliminarGenero
+    @id int
+as
+begin
+    --Verificamos si el género existe
+    if not exists(select 1 from clientes.Genero where id = @id)
+    begin
+        print 'No se encontró el género con el ID especificado.';
+        return;
+    end
+    --Eliminamos el género
+    delete from clientes.Genero where id = @id;
+    print 'Género eliminado correctamente con ID: ' + cast(@id as varchar(4));
+end;
+go
+
+--Creamos el store procedure para marcar como eliminado un cliente
+create or alter procedure clientes.EliminarCliente
+    @id int
+as
+begin
+    --Verificamos si el cliente existe
+    if not exists(select 1 from clientes.Cliente where id = @id)
+    begin
+        print 'No se encontró el cliente con el ID especificado.';
+        return;
+    end
+    --Marcamos como eliminado el cliente
+    update clientes.Cliente set eliminado = 1 where id = @id;
+    print 'Cliente marcado como eliminado correctamente con ID: ' + cast(@id as varchar(4));
+end;
+go
+
+--Creamos los store procedures de eliminacion para el esquema de sucursales
+
+--Creamos el store procedura para marcar como eliminado una sucursal
+create or alter procedure sucursales.EliminarSucursal
+    @id int
+as
+begin
+    --Verificamos si la sucursal existe
+    if not exists(select 1 from sucursales.Sucursal where id = @id and eliminado = 0)
+    begin
+        print 'No se encontró la sucursal con el ID especificado o ya está eliminada.';
+        return;
+    end
+    --Marcamos como eliminida la sucursal
+    update sucursales.Sucursal set eliminado = 1 where id = @id;
+    print 'Sucursal marcada como eliminada correctamente con ID: ' + cast(@id as varchar(4));
+end;
+go
+
+--Creamos el store procedure para eliminar un tipo de cargo
+create or alter procedure sucursales.EliminarTipoDeCargo
+	@id int
+as
+begin
+	--Verificamos si el tipo de cargo existe
+	if not exists (select 1 from sucursales.TipoDeCargo where id = @id)
+	begin
+		print 'No se encontró la sucursal con el ID especificado o ya está eliminada.'
+		return;
+	end
+	--Eliminamos el tipo de cargo
+	delete from sucursales.TipoDeCargo where id = @id
+	print 'Tipo de cargo eliminado correctamene con ID: ' + cast(@id as varchar(4));
+end;
+go
+
+--Creamos el store procedure para eliminar los turnos
+create or alter procedure sucursales.Turno
+	@id int
+as
+begin
+	--Verificamos si el turno existe
+	if not exists (select 1 from sucursales.Turno where id = @id)
+	begin
+		print 'No se encontró el turno con el ID especificado.'
+		return;
+	end
+	--Elminamos el turno
+	delete from sucursales.Turno where id = @id
+	print 'Turno eliminado correctamente con ID: ' + cast(@id as varchar(4));
+end;
+go
+
+--Creamos el store procedure para marcar como eliminado los empleados
+create or alter procedure sucursales.EliminarEmpleado
+    @legajo int
+as
+begin
+    --Verificamos si el empleado existe
+    if not exists(select 1 from sucursales.Empleado where legajo = @legajo and eliminado = 0)
+    begin
+        print 'No se encontró el empleado con el ID especificado o ya está eliminado.';
+        return;
+    end
+    --Marcamos como eliminado el empleado
+    update sucursales.Empleado set eliminado = 1 where legajo = @legajo;
+    print 'Empleado marcado como eliminado correctamente con el ID: ' + cast(@id as varchar(4));
+end;
+go
+
+
