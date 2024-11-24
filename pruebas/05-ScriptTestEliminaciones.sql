beb41454243fe90e79a69c2019f367a6476f711b
Luciano Javier Romano <111657020+romano-luciano@users.noreply.github.com>
2024-11-24 13:46:36 -0300

    Update and rename PruebasStoredProcedures.sql to 05-ScriptTestEliminaciones.sql

---
+++ pruebas/05-ScriptTestEliminaciones.sql
@@ -0,0 +1,31 @@
+--Nos posicionamos en la base de datos
+use g05com2900
+go
+
+--Creamos los test para probar las eliminaciones
+
+--Test para la tabla tipo de cliente
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
